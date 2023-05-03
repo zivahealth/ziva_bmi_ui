db48ce0e5ef5c8254711055a1a5e808f905c25b0
ashish <ashish@PWA300.local>
2023-05-04 02:18:55 +0530

    Adds body fat per

--- component/BMI/BMI.tsx
+++ component/BMI/BMI.tsx
@@ -18,6 +18,7 @@ import axios from "axios";
 
 const LoginFormSchema = Yup.object().shape({
   weight_lbs: Yup.string().required("Please enter the weight."),
+  body_fat_pred: Yup.number().required("Please enter the Body Fat(%)."),
   height_ft: Yup.string().required("Please enter the height in feet."),
   sex_num: Yup.number().required("Select gender."),
   waist_circum_in: Yup.number().required("Enter waist_circum_in."),
@@ -158,6 +159,7 @@ export const BMI = () => {
                         weight_lbs: 140,
                         height_ft: "6'0",
                         sex_num: 0,
+                        body_fat_pred: 0,
                         waist_circum_in: 28,
                         age_yrs: 25,
                       }}
@@ -211,6 +213,18 @@ export const BMI = () => {
                               label="Waist Circum(Inch)"
                               type={"text"}
                               placeholder="Enter Waist Circum in Inch"
+                              errors={errors}
+                              values={values}
+                              touched={touched}
+                              handleChange={handleChange}
+                            ></InputField>
+                          </div>
+                          <div className="form-group">
+                            <InputField
+                              name="body_fat_pred"
+                              label="Body Fat(%)"
+                              type={"number"}
+                              placeholder="Enter Body Fat Percentage"
                               errors={errors}
                               values={values}
                               touched={touched}
